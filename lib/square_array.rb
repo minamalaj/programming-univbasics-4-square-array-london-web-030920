d5af050957db6f3122e8d6503307649870d604e7
Mina Malaj <mina.malaj@hotmail.com>
2020-03-04 17:50:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -14,7 +14,7 @@ def square_array(numbers)
   counter = 0
   sqr_numbers=[]
   while numbers[counter] do
-    sqr_numbers<<numbers[counter]*numbers[counter]
+    sqr_numbers<<numbers[counter]**
     counter += 1
   end
   sqr_numbers
